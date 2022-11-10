c5cd6f190f4eaa660cb8515c23202d568e80bfcd
nnnnadia <tristaonadia@gmail.com>
2022-11-10 18:14:54 -0300

    Requisito 2: <ProductService> - `findAll`

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -5,4 +5,6 @@ export async function create(name: string, amount: string): Promise<IProduct> {
   return ProductModel.create(name, amount);
 }
 
-export const temp = 'só pro linter não reclamar por enquanto';
+export async function findAll(): Promise<IProduct[]> {
+  return ProductModel.findAll();
+}

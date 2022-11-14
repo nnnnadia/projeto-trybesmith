e5d21901b9d234fba0a8b769245b0a95928d364c
nnnnadia <tristaonadia@gmail.com>
2022-11-14 16:01:43 -0300

    Requisito 6: <route> - `productValidation` on POST /products

--- src/routers/product.route.ts
+++ src/routers/product.route.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as ProductController from '../controllers/product.controller';
+import productValidation from '../middlewares/productValidation.middleware';
 
 const router = Router();
 
-router.post('/products', ProductController.create);
+router.post('/products', productValidation, ProductController.create);
 router.get('/products', ProductController.findAll);
 
 export default router;

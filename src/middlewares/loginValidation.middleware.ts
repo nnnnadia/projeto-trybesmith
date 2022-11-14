c8d5868d6b67b522f1b090de28bdf25192732471
nnnnadia <tristaonadia@gmail.com>
2022-11-14 15:27:12 -0300

    Requisito 5: <middleware> - `loginValidation`

---
+++ src/middlewares/loginValidation.middleware.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import ErrorWithStatusCode from '../errors/ErrorWithStatusCode';
+import loginSchema from '../errors/validations/loginSchema';
+
+const loginValidation = (
+  req: Request,
+  _res: Response,
+  next: NextFunction,
+) => {
+  const { username, password } = req.body;
+  const { error } = loginSchema.validate({ username, password });
+  if (error) next(new ErrorWithStatusCode(400, error.message));
+  next();
+};
+
+export default loginValidation;

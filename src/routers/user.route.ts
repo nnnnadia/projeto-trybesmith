1bb308022ad890219f7a53acd77b628e974cd1de
nnnnadia <tristaonadia@gmail.com>
2022-11-14 15:27:51 -0300

    Requisito 5: <route> - POST /login

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import * as UserController from '../controllers/user.controller';
+import loginValidation from '../middlewares/loginValidation.middleware';
 
 const router = Router();
 
+router.post('/login', loginValidation, UserController.login);
 router.post('/users', UserController.create);
 
 export default router;

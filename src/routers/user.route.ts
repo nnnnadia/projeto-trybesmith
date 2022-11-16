0e0e3dab8ce1cf09faea6d94831ad06d4d35f22a
nnnnadia <tristaonadia@gmail.com>
2022-11-16 16:26:14 -0300

    Requisito 7: <route> - `userValidation` on POST /users

--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import * as UserController from '../controllers/user.controller';
 import loginValidation from '../middlewares/loginValidation.middleware';
+import userValidation from '../middlewares/userValidation.middleware';
 
 const router = Router();
 
 router.post('/login', loginValidation, UserController.login);
-router.post('/users', UserController.create);
+router.post('/users', userValidation, UserController.create);
 
 export default router;

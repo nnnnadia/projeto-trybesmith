60e459b4857ae43a920f9ede7ea5fe54bd9716c5
nnnnadia <tristaonadia@gmail.com>
2022-11-17 14:35:58 -0300

    Requisito 8: <OrderModel> - `create`

--- src/models/order.model.ts
+++ src/models/order.model.ts
@@ -1,14 +1,31 @@
-import { RowDataPacket } from 'mysql2';
+import { ResultSetHeader, RowDataPacket } from 'mysql2';
+import DefaultError from '../errors/DefaultError';
 import IOrder from '../interfaces/IOrder';
 import connection from './connection';
 
-export default async function findAll(): Promise<IOrder[]> {
-  const [result] = await connection.execute<RowDataPacket[]>(
-    'SELECT `o`.`id`, `o`.`userId`, JSON_ARRAYAGG(`p`.`id`) AS `productsIds`'
-    + 'FROM `Trybesmith`.`Orders` AS `o`'
-    + 'JOIN `Trybesmith`.`Products` AS `p`'
-    + 'ON `o`.`id` = `p`.`orderId`'
-    + 'GROUP BY `o`.`id`',
-  );
-  return result as IOrder[];
+export async function findAll(): Promise<IOrder[]> {
+  try {
+    const [result] = await connection.execute<RowDataPacket[]>(
+      'SELECT `o`.`id`, `o`.`userId`, JSON_ARRAYAGG(`p`.`id`) AS `productsIds`'
+      + 'FROM `Trybesmith`.`Orders` AS `o`'
+      + 'JOIN `Trybesmith`.`Products` AS `p`'
+      + 'ON `o`.`id` = `p`.`orderId`'
+      + 'GROUP BY `o`.`id`',
+    );
+    return result as IOrder[];
+  } catch (error) {
+    throw new DefaultError('Internal Error');
+  }
+}
+
+export async function create(userId: number): Promise<number> {
+  try {
+    const [{ insertId }] = await connection.execute<ResultSetHeader>(
+      'INSERT INTO `Trybesmith`.`Orders` (`userId`) VALUE (?)',
+      [userId],
+    );
+    return insertId;
+  } catch (error) {
+    throw new DefaultError('Internal Error');
+  }
 }

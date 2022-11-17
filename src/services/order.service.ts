f1e80b582b3a93cf7b14e69f40f9a58d1bec2549
nnnnadia <tristaonadia@gmail.com>
2022-11-17 14:37:28 -0300

    Requisito 8: <OrderService> - `create`

--- src/services/order.service.ts
+++ src/services/order.service.ts
@@ -1,6 +1,15 @@
 import IOrder from '../interfaces/IOrder';
-import OrderModelFindAll from '../models/order.model';
+import * as OrderModel from '../models/order.model';
+import * as ProductModel from '../models/product.model';
+import * as jwt from '../utils/jwt';
 
-export default async function findAll(): Promise<IOrder[]> {
-  return OrderModelFindAll();
+export async function findAll(): Promise<IOrder[]> {
+  return OrderModel.findAll();
+}
+
+export async function create(tokenJWT: string, productsIds: number[]) {
+  const { userId } = jwt.retrieveTokenData(tokenJWT);
+  const orderId = await OrderModel.create(userId);
+  await ProductModel.assignOrder(orderId, productsIds);
+  return { userId, productsIds };
 }

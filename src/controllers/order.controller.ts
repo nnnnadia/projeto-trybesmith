455a381618e65a65818e3ea42b0d9e98b0d3bc7d
nnnnadia <tristaonadia@gmail.com>
2022-11-17 14:38:24 -0300

    Requisito 8: <OrderController> - `create`

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from 'express';
-import OrderServiceFindAll from '../services/order.service';
+import * as OrderService from '../services/order.service';
 
-export default async function findAll(
+export async function findAll(
   _req: Request,
   res: Response,
 ): Promise<void> {
-  const orders = await OrderServiceFindAll();
+  const orders = await OrderService.findAll();
   res.status(200).json(orders);
+}
+
+export async function create(
+  req: Request,
+  res: Response,
+) {
+  const { authorization } = req.headers;
+  const { productsIds } = req.body;
+  const order = await OrderService.create(authorization as string, productsIds);
+  res.status(201).json(order);
 }
